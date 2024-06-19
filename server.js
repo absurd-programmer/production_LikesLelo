b4bce96481be426dd2388c517c8b24fdfd9e2ab7
nikhil <nikhil@gmail.com>
2024-06-19 16:13:33 +0530

    Final Code
    finalising the code for production

---
+++ server.js
@@ -0,0 +1,55 @@
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import mongoose from "mongoose";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import cors from "cors";
+import path from "path";
+
+// Configure environment variables
+dotenv.config();
+
+// Database configuration
+connectDB();
+
+// Initialize express app
+const app = express();
+
+// Middleware setup
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Your frontend origin
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+// Route setup
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
+
+//route rest api
+app.use("*", function (req, res) {
+  res.sendFile(__dirname, "./client/build/index.html");
+});
+
+// Define port
+const PORT = process.env.PORT || 8080;
+
+// Log MongoDB URL for verification
+console.log(`MONGO_URL is ${process.env.MONGO_URL}`.bgYellow.white);
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(
+    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
+      .white
+  );
+});
